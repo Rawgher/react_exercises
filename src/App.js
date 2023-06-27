e5f35fcfc09f1d0d5b4616e9ef48f942b0289dc4
Roger Pender <rdpender3@gmail.com>
2023-06-27 10:56:18 -0700

    date counter project added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import FlashCards from './components/FlashCards/FlashCards';
 import './App.css';
+import DateCounterV2 from './components/DateCounterV2/DateCounterV2';
+import DateCounterV1 from './components/DateCounterV1/DateCounterV1';
 
 function App() {
   return (
     <div className="App">
       <FlashCards />
+      <div className="linebreak"></div>
+      <DateCounterV1 />
+      <div className="linebreak"></div>
+      <DateCounterV2 />
     </div>
   );
 }
---
+++ src/components/DateCounterV2/DateCounterV2.js
@@ -0,0 +1,9 @@
+function DateCounterV2() {
+  return (
+    <div>
+      <h1>Date Counter V2</h1>
+    </div>
+  );
+}
+
+export default DateCounterV2;
