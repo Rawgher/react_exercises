50bc07ccd68d6fb450e15a8350a56749064484fe
Roger Pender <rdpender3@gmail.com>
2023-06-27 10:49:09 -0700

    initial push and flashcard app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FlashCards from './components/FlashCards/FlashCards';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+export default App;
